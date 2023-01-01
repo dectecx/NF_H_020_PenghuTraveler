2f9f8653bc98f711d48eb1413cab040c00e57f60
ts6075 <75653667+ts6075@users.noreply.github.com>
2023-01-02 01:21:54 +0800

    Layout套版

---
+++ PenghuTraveler/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PenghuTraveler.Controllers
+{
+    public class BlogController : Controller
+    {
+        /// <summary>
+        /// 單篇文章
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Single()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 取得留言板列表資料
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetMessageBoard(string id)
+        {
+            return View();
+        }
+    }
+}
